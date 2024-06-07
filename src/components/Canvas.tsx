4455f5f42c3567483073397528f575223e475db0
Franc Camps Febrer <francamps@gmail.com>
2024-06-07 15:51:09 -0400

    update site

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -15,175 +15,147 @@ import FindSurfaces from "@/assets/threejs/addons/FindSurfaces.js";
 import styles from "@/components/Canvas.module.css";
 
 const Canvas = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    const requestRef = useRef<any>(null);
-    const composerRef = useRef<any>(null);
-    const cameraRef = useRef<any>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const requestRef = useRef<any>(null);
+  const composerRef = useRef<any>(null);
+  const cameraRef = useRef<any>(null);
 
-    const width = 600; //window.innerWidth;
-    const height = 600; //window.innerHeight;
-    const scene = new THREE.Scene();
+  const width = 300; //window.innerWidth;
+  const height = 300; //window.innerHeight;
+  const scene = new THREE.Scene();
+  const renderer = useRef<THREE.WebGLRenderer>();
+  const effectFXAA = useRef();
+  const customOutline = useRef();
 
-    //Add Renderer
-    useEffect(() => {
-        if (canvasRef.current) {
-            const canvas = canvasRef.current;
-            const renderer = new THREE.WebGLRenderer({
-                antialias: true,
-                canvas,
-                alpha: true,
-            });
-            renderer.setClearColor(0x000000, 0); // the default
-            renderer.setSize(width, height);
-            renderer.setPixelRatio(window.devicePixelRatio);
+  //Add Renderer
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      renderer.current = new THREE.WebGLRenderer({
+        antialias: true,
+        canvas,
+        alpha: true,
+      });
+      renderer.current.setClearColor(0x000000, 0); // the default
+      renderer.current.setSize(width, height);
+      renderer.current.setPixelRatio(window.devicePixelRatio);
 
-            //add Camera
-            cameraRef.current = new THREE.PerspectiveCamera(
-                75,
-                width / height,
-                0.1,
-                1000
-            );
+      //add Camera
+      cameraRef.current = new THREE.PerspectiveCamera(
+        75,
+        width / height,
+        0.1,
+        1000
+      );
 
-            cameraRef.current.position.x = -2.7666651976750187;
-            cameraRef.current.position.y = 2.4301866055440726;
-            cameraRef.current.position.z = -4.670222058565317;
+      cameraRef.current.position.x = -2.7666651976750187;
+      cameraRef.current.position.y = 2.4301866055440726;
+      cameraRef.current.position.z = -4.670222058565317;
 
-            cameraRef.current.lookAt(0, 0, 0);
+      cameraRef.current.lookAt(0, 0, 0);
 
-            //Camera Controls
-            //const controls = new OrbitControls(
-            //    cameraRef.current,
-            //    renderer.domElement
-            //);
-            //
-            //controls.target.x = -0.032275654135816935;
-            //controls.target.y = 2.0338707166512435;
-            //controls.target.z = -0.19146553019782225;
+      //Camera Controls
+      //const controls = new OrbitControls(
+      //    cameraRef.current,
+      //    renderer.domElement
+      //);
+      //
+      //controls.target.x = -0.032275654135816935;
+      //controls.target.y = 2.0338707166512435;
+      //controls.target.z = -0.19146553019782225;
 
-            // Set up post processing
-            // Create a render target that holds a depthTexture so we can use it in the outline pass
-            // See: https://threejs.org/docs/index.html#api/en/renderers/WebGLRenderTarget.depthBuffer
-            // @ts-expect-error
-            const depthTexture = new THREE.DepthTexture();
-            const renderTarget = new THREE.WebGLRenderTarget(width, height, {
-                depthTexture: depthTexture,
-                depthBuffer: true,
-            });
+      // Set up post processing
+      // Create a render target that holds a depthTexture so we can use it in the outline pass
+      // See: https://threejs.org/docs/index.html#api/en/renderers/WebGLRenderTarget.depthBuffer
+      // @ts-expect-error
+      const depthTexture = new THREE.DepthTexture();
+      const renderTarget = new THREE.WebGLRenderTarget(width, height, {
+        depthTexture: depthTexture,
+        depthBuffer: true,
+      });
 
-            // Initial render pass.
-            composerRef.current = new EffectComposer(renderer, renderTarget);
-            //const pass = new RenderPass(scene, cameraRef.current);
-            //composerRef.current.addPass(pass);
+      // Initial render pass.
+      composerRef.current = new EffectComposer(renderer.current, renderTarget);
+      //const pass = new RenderPass(scene, cameraRef.current);
+      //composerRef.current.addPass(pass);
 
-            // Outline pass.
-            const customOutline = new CustomOutlinePass(
-                new THREE.Vector2(width, height),
-                scene,
-                cameraRef.current
-            );
-            composerRef.current.addPass(customOutline);
+      // Outline pass.
+      customOutline.current = new CustomOutlinePass(
+        new THREE.Vector2(width, height),
+        scene,
+        cameraRef.current
+      );
+      composerRef.current.addPass(customOutline.current);
 
-            // Antialias pass.
-            const effectFXAA = new ShaderPass(FXAAShader);
-            effectFXAA.uniforms["resolution"].value.set(1 / width, 1 / height);
-            composerRef.current.addPass(effectFXAA);
+      // Antialias pass.
+      effectFXAA.current = new ShaderPass(FXAAShader);
+      effectFXAA.current?.uniforms["resolution"].value.set(
+        1 / width,
+        1 / height
+      );
+      composerRef.current.addPass(effectFXAA.current);
 
-            renderer.setPixelRatio(window.devicePixelRatio);
-            composerRef.current.setPixelRatio(window.devicePixelRatio);
+      renderer.current?.setPixelRatio(window.devicePixelRatio);
+      composerRef.current.setPixelRatio(window.devicePixelRatio);
 
-            const surfaceFinder = new FindSurfaces();
+      const surfaceFinder = new FindSurfaces();
 
-            const loader = new GLTFLoader();
+      const loader = new GLTFLoader();
 
-            // Load a glTF resource
-            loader.load(
-                // resource URL
-                "assets/talaiot.gltf",
-                // called when the resource is loaded
-                function (gltf) {
-                    scene.add(gltf.scene);
+      // Load a glTF resource
+      loader.load(
+        // resource URL
+        "assets/talaiot.gltf",
+        // called when the resource is loaded
+        function (gltf) {
+          scene.add(gltf.scene);
 
-                    surfaceFinder.surfaceId = 0;
-                    scene.traverse((node) => {
-                        if (node.type == "Mesh") {
-                            const colorsTypedArray =
-                                surfaceFinder.getSurfaceIdAttribute(node);
-                            // @ts-expect-error
-                            node.geometry.setAttribute(
-                                "color",
-                                new THREE.BufferAttribute(colorsTypedArray, 4)
-                            );
-                        }
-                    });
-
-                    customOutline.updateMaxSurfaceId(
-                        surfaceFinder.surfaceId + 1
-                    );
-                }
-            );
-        }
-    }, [canvasRef]);
-
-    const animate = useCallback(
-        (time: number) => {
-            if (composerRef.current) {
-                cameraRef.current.position.set(
-                    10 * Math.sin(time / 900),
-                    2.4301866055440726,
-                    10 * Math.cos(time / 900)
-                );
-                cameraRef.current.lookAt(0, 0, 0);
-                // The 'state' will always be the initial value here
-                requestRef.current = requestAnimationFrame(animate);
-                composerRef.current.render();
+          surfaceFinder.surfaceId = 0;
+          scene.traverse((node) => {
+            if (node.type == "Mesh") {
+              const colorsTypedArray =
+                surfaceFinder.getSurfaceIdAttribute(node);
+              // @ts-expect-error
+              node.geometry.setAttribute(
+                "color",
+                new THREE.BufferAttribute(colorsTypedArray, 4)
+              );
             }
-        },
-        [composerRef]
-    );
+          });
 
-    useEffect(() => {
-        requestRef.current = requestAnimationFrame(animate);
-        return () => cancelAnimationFrame(requestRef.current);
-    }, [animate]); // Make sure the effect runs only once
-
-    useEffect(() => {
-        function onWindowResize() {
-            cameraRef.current.aspect = window.innerWidth / window.innerHeight;
-            cameraRef.current.updateProjectionMatrix();
-
-            // @ts-expect-error
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            composerRef.current.setSize(window.innerWidth, window.innerHeight);
-            // @ts-expect-error
-            effectFXAA.setSize(window.innerWidth, window.innerHeight);
-            // @ts-expect-error
-            customOutline.setSize(window.innerWidth, window.innerHeight);
-
-            // @ts-expect-error
-            renderer.setPixelRatio(window.devicePixelRatio);
-            composerRef.current.setPixelRatio(window.devicePixelRatio);
-
-            // @ts-expect-error
-            effectFXAA.uniforms["resolution"].value.set(
-                1 / window.innerWidth,
-                1 / window.innerHeight
-            );
+          customOutline.current.updateMaxSurfaceId(surfaceFinder.surfaceId + 1);
         }
-        window.addEventListener("resize", onWindowResize, false);
+      );
+    }
+  }, [canvasRef]);
 
-        return () => {
-            window.removeEventListener("resize", onWindowResize);
-        };
-    }, []);
+  const animate = useCallback(
+    (time: number) => {
+      if (composerRef.current) {
+        cameraRef.current.position.set(
+          10 * Math.sin(time / 600),
+          1.4301866055440726,
+          10 * Math.cos(time / 600)
+        );
+        cameraRef.current.lookAt(0, 0, 0);
+        // The 'state' will always be the initial value here
+        requestRef.current = requestAnimationFrame(animate);
+        composerRef.current.render();
+      }
+    },
+    [composerRef]
+  );
 
-    return (
-        <div className={styles.canvas}>
-            <h2>Sa Talaia</h2>
-            <h4>Intelligent world building</h4>
-            <canvas ref={canvasRef}></canvas>
-        </div>
-    );
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [animate]); // Make sure the effect runs only once
+
+  return (
+    <div className={styles.canvas}>
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
 };
 
 export default Canvas;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,22 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import { useState } from "react";
-
-import Header from "@/components/Header";
 import Banner from "@/components/Banner";
 import Canvas from "@/components/Canvas";
 
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Sa Talaia</title>
-                <meta name="description" content="Sa Talaia" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className={styles.main}>
-                {false && <Header />}
-                <Banner />
-                <Canvas />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Sa Talaia</title>
+        <meta name="description" content="Sa Talaia" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <Banner />
+        <Canvas />
+      </main>
+    </>
+  );
 }
