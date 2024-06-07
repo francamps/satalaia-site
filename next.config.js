198dc961ebdac90ef561c4dee43a59fceb216342
Franc Camps Febrer <francamps@gmail.com>
2024-06-07 16:06:16 -0400

    Fonts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.resolve(__dirname, "src/assets/fonts"),
+              to: path.resolve(__dirname, ".next/static/fonts"),
+            },
+          ],
+        })
+      );
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
